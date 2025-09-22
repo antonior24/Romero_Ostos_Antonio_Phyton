c9276dd50f01a89d11f0a997a54adaaad7ca88b9
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-09-22 14:37:20 +0200

    v1

---
+++ clase.py
@@ -0,0 +1,16 @@
+class animal:
+    def __init__(self, nombre = "", especie = ""):
+        self.nombre = nombre
+        self.especie = especie
+
+    def hacer_sonido(self, sonido):
+        return f"{self.nombre} dice {sonido}"
+    
+animal1 = animal("Rex", "Perro")
+animal2 = animal("Michi", "Gato")
+
+print(animal1.hacer_sonido("Guau"))
+print(animal2.hacer_sonido("Miau"))
+print (animal1.nombre)    
+
+print (animal2.especie)
