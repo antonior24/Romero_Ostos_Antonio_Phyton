b648e826f531ba5c6e4fb1865d712fea87ab3410
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-09-22 14:44:52 +0200

    v3

--- si.py
+++ si.py
@@ -1,5 +1,7 @@
 numero = 17
-if numero >= 18:
-    print ("Eres mayor de edad")
-else:
+if numero > 27:
+    print ("Eres mayor bastane grande")
+elif numero < 18:
     print ("Eres menor de edad")
+else:
+    print ("Eres mayor de edad")
