e456a1e16df426ddb7da87c36a64eb05f99ddc37
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-09-22 13:40:42 +0200

    v1

---
+++ tipo.py
@@ -0,0 +1,20 @@
+num = 5
+print (num)
+
+dec = 5.5
+print (dec)
+
+bool = True
+print (bool)
+
+cadena = "hola"
+print (cadena)
+
+lista = [1, 2, 3, 4, 5]
+print (lista)
+
+lista[2] = 6
+print (lista[3])
+
+diccionario = {"nombre": "Antonio", "edad": 20}
+print (diccionario["edad"])
