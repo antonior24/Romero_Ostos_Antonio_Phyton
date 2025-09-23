c9b85b74859b3d99cc5fdb5e3a926366c325b3b1
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-09-23 09:00:59 +0200

    v3

--- clase.py
+++ clase.py
@@ -6,6 +6,16 @@ class animal:
     def hacer_sonido(self, sonido):
         return f"{self.nombre} dice {sonido}"
     
+    def __str__(self):
+        return f"Animal(nombre={self.nombre}, especie={self.especie})"
+    
+    def mostrar_nombre(self):
+        return self.nombre
+    
+class perro(animal):
+    def __init__(self, nombre = "", especie = "Perro"):
+        super().__init__(nombre, especie)
+    
 animal1 = animal("Rex", "Perro")
 animal2 = animal("Michi", "Gato")
 
@@ -15,3 +25,4 @@ print (animal1.nombre)
 
 print (animal2.especie)
 print (animal1.hacer_sonido("Woof"))
+print (animal2.__str__())
