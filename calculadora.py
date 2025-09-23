eb67881c343111f7e7e395e0438207ce82639cec
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-09-23 10:19:40 +0200

    v1

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+class Calculadora:
+    def __init__(self, a="", b=""):
+        self.a = a
+        self.b = b
+    
+    def sumar(a, b):
+        return a + b
+
+    def restar(self, a, b):
+        return a - b
+
+    def multiplicar(self, a, b):
+        return a * b
+
+    def dividir(self, a, b):
+        if b == 0:
+            raise ValueError("No se puede dividir por cero")
+        return a / b
+    
+print(Calculadora.sumar(5, 3))
+print(Calculadora().restar(5, 3))
+print(Calculadora().multiplicar(5, 3))
+print(Calculadora().dividir(5, 0))
+print(Calculadora().dividir(5, 2))
