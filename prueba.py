c70f5407951109240ae965de1d0d62fdd32e6d72
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-09-23 11:05:34 +0200

    v3

--- prueba.py
+++ prueba.py
@@ -1,11 +1,17 @@
 import unittest
 from calculadora import Calculadora
 class TestCalculadora(unittest.TestCase):
+    
+    def setUp(self):
+        print("Iniciando pruebas de la calculadora")
+        self.calculadoraset = Calculadora(5, 3)
+    
     def test_sumar(self):
         print("Prueba de suma correcta")
         calculadora = Calculadora(8,2)
-        self.assertNotEqual(calculadora.sumar(), 10)
+        self.assertNotEqual(calculadora.sumar(), 11)
         self.assertEqual(calculadora.sumar(), 10)
+        self.assertEqual(self.calculadoraset.sumar(), 8)
     
     def test_restar(self):
         calculadora = Calculadora(5, 3)
